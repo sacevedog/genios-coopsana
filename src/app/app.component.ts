2a290260c89bfd1f856f1bcc9235af07529ce811
Santiago Acevedo <sastiag@gmail.com>
2022-10-27 15:50:35 -0500

    Modificación de diseño e incorporación de angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'genios-coopsana';
+  title = 'Los genios 😎';
+
+  data: any[] = [];
+
+  constructor(
+    private http: HttpClient
+  ){}
+
+  ngOnInit(){
+    this.http.get('https://apps.coopsana.co:8766/api-prueba-tecnica-coopsana/desarrollo-tecnologico/genios', {headers:{user:'santiago.acevedo',pass:'coopsana'}})
+    .subscribe((data: any) => {
+      this.data = data.genios;
+      console.log(data.genios)
+      
+    })
+  }
+
 }
